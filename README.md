6d826024300d9c50113189de03b7ea00808169c5
austinloveless <austinloveless5171@gmail.com>
2021-10-22 21:06:58 -0600

    fix: major refactor to get deployed to lambda

--- README.md
+++ README.md
@@ -2,15 +2,33 @@
 
 Serverless Typescript RESTful API returning information based on specified GitHub Repos.
 
+## Production Testing
+
+As of October 22 2021 this application is live this is subject to change.
+
+Base URL: https://pkk06vpt98.execute-api.us-east-1.amazonaws.com/prod/
+
+- Info on a given repo: `github/pulls/{owner}/{repo}`
+  (e.g. https://pkk06vpt98.execute-api.us-east-1.amazonaws.com/prod/github/pulls/facebook/jest)
+
+- Numbers of Commits for all Pull requests in a given repo: `github/pulls/{owner}/{repo}/commits`
+  (e.g. https://pkk06vpt98.execute-api.us-east-1.amazonaws.com/prod/github/pulls/facebook/jest/commits)
+
+- Number of commits for a specific pull request in a given repo: `github/pulls/{owner}/{repo}/{pull_number}/commits` (e.g. https://pkk06vpt98.execute-api.us-east-1.amazonaws.com/prod/github/pulls/facebook/jest/11988/commits)
+
 ## Getting started.
 
 ---
 
-**NOTE**
+**NOTE:**
 GitHub throttles API request at 60 per hour. To get around this follow this [guide](https://docs.github.com/en/rest/guides/basics-of-authentication) and replace the value in .env.example to your GitHub Access Code.
+
+THIS IS AN OPTIONAL STEP.
 
 ---
 
+- `npm ci`
+- `cd app`
 - `cp .env.example .env`
 - `npm ci`
 - `npm start`
@@ -19,6 +37,7 @@ GitHub throttles API request at 60 per hour. To get around this follow this [gui
 
 Tests are written with Jest and Supertest.
 
+- `cd app/`
 - `npm run test`
 
 ## Deploying to AWS Lambda
--- cdk/lib/api.stack.ts
+++ cdk/lib/api.stack.ts
@@ -1,44 +1,54 @@
-import { Stack, Construct, CfnOutput } from "@aws-cdk/core";
-import { LambdaRestApi, ResourceBase, RestApi } from "@aws-cdk/aws-apigateway";
+import { Stack, Construct, CfnOutput, Duration } from "@aws-cdk/core";
+import { LambdaRestApi } from "@aws-cdk/aws-apigateway";
 import { Function, Runtime, Code } from "@aws-cdk/aws-lambda";
-import { ISecret, Secret } from "@aws-cdk/aws-secretsmanager";
+import { Secret } from "@aws-cdk/aws-secretsmanager";
 
 export class GraphqlApiStack extends Stack {
   readonly handler: any;
-  readonly api: RestApi;
-  readonly apiPathOutput: CfnOutput;
-  readonly graphql: ResourceBase;
-  readonly gitHubAccessToken: ISecret;
 
   constructor(scope: Construct, id: string, props: {}) {
     super(scope, id, props);
 
-    this.gitHubAccessToken = Secret.fromSecretNameV2(
+    const gitHubAccessToken = Secret.fromSecretNameV2(
       this,
       "GITHUB_ACCESS_TOKEN",
       "GITHUB_ACCESS_TOKEN"
     );
 
-    this.handler = new Function(this, "graphql", {
+    const handler = new Function(this, "graphql", {
       runtime: Runtime.NODEJS_14_X,
-      code: Code.fromAsset("./"),
-      handler: "dist/src/lambda.handler",
+      code: Code.fromAsset("app"),
+      handler: "dist/lambda.handler",
+      timeout: Duration.seconds(30),
 
       environment: {
-        GITHUB_ACCESS_TOKEN: this.gitHubAccessToken.secretValue.toString(),
+        GITHUB_ACCESS_TOKEN: gitHubAccessToken.secretValue.toString(),
       },
     });
 
-    this.api = new LambdaRestApi(this, "graphql-api", {
-      handler: this.handler,
+    const api = new LambdaRestApi(this, "graphql-api", {
+      handler,
       proxy: false,
     });
 
-    this.graphql = this.api.root.addResource("graphql");
-    this.graphql.addMethod("ANY");
+    api.root.addMethod("GET");
 
-    this.apiPathOutput = new CfnOutput(this, "apiPath", {
-      value: this.api.root.path,
+    const github = api.root.addResource("github");
+    const githubPull = github.addResource("pulls");
+    const githubPullOwner = githubPull.addResource("{owner}");
+    const githubPullRepo = githubPullOwner.addResource("{repo}");
+    githubPullRepo.addMethod("GET");
+
+    const githubPullCommits = githubPullRepo.addResource("commits");
+    githubPullCommits.addMethod("GET");
+
+    const githubPullPullNumber = githubPullRepo.addResource("{pull_number}");
+    const githubPullPullNumberCommits =
+      githubPullPullNumber.addResource("commits");
+    githubPullPullNumberCommits.addMethod("GET");
+
+    new CfnOutput(this, "apiPath", {
+      value: api.root.path,
       description: "Path of the API",
     });
   }
